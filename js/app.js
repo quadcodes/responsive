7686b8c9a5ba99d9f7f7e914bb4972817370b1fd
Oldrich Vetesnik <mrmil@igloonet.cz>
2012-02-28 21:49:36 +0100

    Revamped!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-$(document).ready(function(){
-  //focusOnUrlField();
-  doIt();
+$(document).ready(function() {
+  focusOnUrlField();
+  initReloader();
+  initProportionsChanger();
 });
 
 function focusOnUrlField()
@@ -8,16 +9,16 @@ function focusOnUrlField()
   $('#url').focus();
 }
 
-function doIt()
+function initReloader()
 {
   var form         = $('form'),
       urlInput     = $('#url'),
       deviceSelect = $('#device'),
-      iframes      = $('iframe'),
+      iframe       = $('iframe'),
       proxyUrl     = 'http://proxy.oldrichvetesnik.cz/'; // Please use your own proxy if possible.
       //proxyUrl     = 'http://127.0.0.1:4567/'; // Uncomment this if you use local proxy.
 
-  form.submit(function(e){
+  form.submit(function(e) {
     e.preventDefault();
     var url    = urlInput.val(),
         device = deviceSelect.val();
@@ -26,8 +27,42 @@ function doIt()
     if ( !url.match(/^https?:\/\//) ) url = 'http://' + url;
     if ( device && proxyUrl ) url = proxyUrl + '?url=' + escape(url) + '&device=' + escape(device);
 
-    iframes.each(function(){
-      $(this).attr('src', url);
-    });
+    iframe.attr('src', url);
+  });
+}
+
+function initProportionsChanger()
+{
+  $('.switches a').click(function(e) {
+    e.preventDefault();
+    var link   = $(this),
+        frame  = $('.frame'),
+        iframe = frame.find('iframe');
+
+    // Only switch width & height
+    if ( link.hasClass('orientation') )
+    {
+      var width  = iframe.attr('height'),
+          height = iframe.attr('width'),
+          text   = width + ' \u00d7 ' + height;
+    }
+    // Change the resolution
+    else
+    {
+      // Mark current item
+      link.parents('ul').find('a.resolution').removeClass('active');
+      link.addClass('active');
+
+      var width  = link.attr('data-width'),
+          height = link.attr('data-height'),
+          text   = link.html();
+    }
+
+    frame.css('width', width);
+    iframe.css({ width: width, height: height });
+    frame.find('h2').html(text);
+    iframe.attr('width', width);
+    iframe.attr('height', height);
+    document.title = document.title.replace(/\(.*\)/, '(' + text + ')');
   });
 }
